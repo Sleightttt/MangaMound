2b65cfa44f8332977725d19a229deedc15e373ad
Sleightttt <brennancota@yahoo.com>
2024-06-04 04:09:51 -0700

    added cleaner nav

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons"; // for icons
 
-export default function App() {
+import Discover from "./components/Discover";
+import Profile from "./components/Profile";
+import Favorites from "./components/Favorites";
+
+const Tab = createBottomTabNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => {
+            let iconName;
+
+            if (route.name === "Discover") {
+              iconName = "compass-outline";
+            } else if (route.name === "Profile") {
+              iconName = "account-outline";
+            } else if (route.name === "Favorites") {
+              iconName = "heart-outline";
+            }
+
+            return (
+              <MaterialCommunityIcons name={iconName} size={24} color={color} />
+            );
+          },
+        })}
+      >
+        <Tab.Screen name="Discover" component={Discover} />
+        <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Screen name="Favorites" component={Favorites} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
