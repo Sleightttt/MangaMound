4145cfc1110aa2e1fb7750daac5145f60abfc92b
Sleightttt <brennancota@yahoo.com>
2025-08-08 19:55:24 -0700

    added users, manga, chapters, comments, genres, readingProgress, notifications, donations, and banners collections to firestore

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -3,21 +3,44 @@ import { View, TextInput, Button, Text, StyleSheet } from "react-native";
 
 const ProfileScreen = () => {
   const [name, setName] = useState("John Doe");
+  const [favAnime, setFavAnime] = useState("Two Piece");
+  const [favorites, setFavorites] = useState("Three Piece");
+  const [memSince, setMemSince] = useState("2016");
+  const [dono, setDono] = useState("1 dorra");
 
-  const handleSave = () => {};
+  const handleSave = () => {
+    setName(name);
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.label}>Name</Text>
       <TextInput style={styles.input} value={name} onChangeText={setName} />
+
       <Text style={styles.label}>Favorite Anime</Text>
-      <TextInput style={styles.input} value={name} onChangeText={setName} />
+      <TextInput
+        style={styles.input}
+        value={favAnime}
+        onChangeText={setFavAnime}
+      />
+
       <Text style={styles.label}>Favorites</Text>
-      <TextInput style={styles.input} value={name} onChangeText={setName} />
+      <TextInput
+        style={styles.input}
+        value={favorites}
+        onChangeText={setFavorites}
+      />
+
       <Text style={styles.label}>Member Since</Text>
-      <TextInput style={styles.input} value={name} onChangeText={setName} />
+      <TextInput
+        style={styles.input}
+        value={memSince}
+        onChangeText={setMemSince}
+      />
+
       <Text style={styles.label}>Donations Made</Text>
-      <TextInput style={styles.input} value={name} onChangeText={setName} />
+      <TextInput style={styles.input} value={dono} onChangeText={setDono} />
+
       <Button title="Save" onPress={handleSave} />
     </View>
   );
